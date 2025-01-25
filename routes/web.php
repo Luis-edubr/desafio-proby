1b3cf321a4e4ada67e2adb81263c441636d68a83
Luis-edubr <moliveiraluiseudardo@gmail.com>
2025-01-25 17:15:06 -0300

    Alteração das rotas pra funcionarem as chamadas de API feitas pelas views aos controllers

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', [ProjectsController::class, 'index'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        Route::get('/create', [ProjectsController::class, 'create'])->name('create');
+        Route::post('/store', [ProjectsController::class, 'store'])->name('store');
+        Route::delete('/{id}', [ProjectsController::class, 'destroy'])->name('destroy');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
