ec7ed8f684c6c40849c56f4d7a248b5432a7b29f
Luis-edubr <moliveiraluiseudardo@gmail.com>
2025-01-25 17:05:34 -0300

    Ajustando o método store pra criar um novo projeto, além de também validar a entrada (que será validada tanto no frontend quanto no backend) e criando também o método de delete, pra deletar um registro(projeto) da tabela

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -1,28 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Projects;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $projects = Projects::paginate(10);
         return view('dashboard.index', compact('projects'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('dashboard.create');
-    } 
-    
-    public function store(Request $request){
-        // $request->validate([
-        //     // 'title' => 'required',
-        //     // 'description' => 'required',
-        //     // 'url' => 'required',
-        // ]);
-
-        // Projects::create($request->all());
-        return redirect()->route('dashboard.index');
     }
+
+    public function store(Request $request)
+    {
+        // Validação dos dados
+        $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'start_date' => 'required|date_format:Y-m-d',
+            'status' => 'required|in:Pendente,Em andamento,Concluído',
+        ]);
+
+        Projects::create($request->all());
+
+        return redirect()->route('dashboard')->with('success', 'Projeto criado com sucesso!');
+    }
+
+
 }
