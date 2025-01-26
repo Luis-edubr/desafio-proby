a6284789d0019a6d97dc84c1bc13b49235cc01af
Luis-edubr <moliveiraluiseudardo@gmail.com>
2025-01-25 23:14:39 -0300

    Criando os métodos show, edit, update e destroy para, respectivamente: Mostrar os detalhes de um projeto em específico, mostrar o formulário com os dados já existentes de um projeto especifico e poder alterá-lo, fazer o update de fato validando os campos e então atualizando-os na db e, por fim, excluindo um registro da db

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -20,7 +20,6 @@ class ProjectsController extends Controller
 
     public function store(Request $request)
     {
-        // Validação dos dados
         $request->validate([
             'name' => 'required|max:255',
             'description' => 'nullable',
@@ -31,6 +30,33 @@ class ProjectsController extends Controller
         Projects::create($request->all());
 
         return redirect()->route('dashboard')->with('success', 'Projeto criado com sucesso!');
+    }
+
+    public function show($id)
+    {
+        $project = Projects::findOrFail($id);
+        return view('dashboard.show', compact('project'));
+    }
+
+    public function edit($id)
+    {
+        $project = Projects::findOrFail($id);
+        return view('dashboard.edit', compact('project'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'start_date' => 'required|date_format:Y-m-d',
+            'status' => 'required|in:Pendente,Em andamento,Concluído',
+        ]);
+
+        $project = Projects::findOrFail($id);
+        $project->update($request->all());
+
+        return redirect()->route('dashboard')->with('success', 'Projeto atualizado com sucesso!');
     }
 
     public function destroy($id){
