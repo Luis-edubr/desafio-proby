a87c673b392559927dcc71f85298bd598eb4887b
Luis-edubr <moliveiraluiseudardo@gmail.com>
2025-01-26 17:32:31 -0300

    Ajustando a view show pra mostrar os documentos pertencentes ao projeto

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -9,6 +9,12 @@
             <div class="card-body">
                 <x-project-form :action="route('dashboard.show', $project->id)" :project="$project" readonly />
             </div>
+            <div class="card-header bg-primary text-white">
+                <strong>Documentos do Projeto</strong>
+            </div><br />
+            <div class="card-body">
+                <x-documents :documents="$documents" />
+            </div>
         </div>
     </div>
 @endsection
