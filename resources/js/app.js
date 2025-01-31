bb204fa7991e978b3af05d04f55a725d0f349a96
Luis-edubr <moliveiraluiseudardo@gmail.com>
2025-01-31 09:04:36 -0300

    Incluindo a logica de newdoc.js em um bloco if pra resolver o bug do dropdown do botão do perfil na pagina inicial do dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+import './bootstrap.js'
 import Alpine from 'alpinejs';
 import './validateDate.js';
 import './confirmDelete.js';
--- resources/js/newDoc.js
+++ resources/js/newDoc.js
@@ -1,10 +1,12 @@
 const addFileInputBtn = document.getElementById('addFileInput');
 const fileInputsContainer = document.getElementById('fileInputsContainer');
 
-addFileInputBtn.addEventListener('click', () => {
-    const newInput = document.createElement('input');
-    newInput.type = 'file';
-    newInput.name = 'Documento[]';
-    newInput.classList.add('form-control', 'mb-2');
-    fileInputsContainer.appendChild(newInput);
-});
+if (addFileInputBtn && fileInputsContainer) {
+    addFileInputBtn.addEventListener('click', () => {
+        const newInput = document.createElement('input');
+        newInput.type = 'file';
+        newInput.name = 'Documento[]';
+        newInput.classList.add('form-control', 'mb-2');
+        fileInputsContainer.appendChild(newInput);
+    });
+}
